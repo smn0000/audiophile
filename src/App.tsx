25a9ed9d0f8ee1c07961002e47997dfe40009fa9
Szymon Strzelec <strzelecszymon.3@gmail.com>
2023-05-15 16:20:24 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./scss/global.scss"
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./navbar/Navbar"
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Routes></Routes>
+    </>
+  )
+}
+
+export default App
